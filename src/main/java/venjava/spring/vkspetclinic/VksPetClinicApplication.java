2dded8345437408a8da063929b7b1db192c543eb
Venka1995 <Venkateshaks598@gmail.com>
2021-08-30 21:53:40 +0530

    Initial commit...project created

---
+++ src/main/java/venjava/spring/vkspetclinic/VksPetClinicApplication.java
@@ -0,0 +1,13 @@
+package venjava.spring.vkspetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VksPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VksPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/venjava/spring/vkspetclinic/VksPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package venjava.spring.vkspetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VksPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
