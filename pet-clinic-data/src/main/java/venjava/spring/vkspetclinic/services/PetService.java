034464d85491aef183d69833f724614018956e15
Venka1995 <Venkateshaks598@gmail.com>
2021-09-08 22:56:56 +0530

    updated by adding common interface

--- pet-clinic-data/src/main/java/venjava/spring/vkspetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/venjava/spring/vkspetclinic/services/PetService.java
@@ -2,11 +2,7 @@ package venjava.spring.vkspetclinic.services;
 
 import venjava.spring.vkspetclinic.model.Pet;
 
-import java.util.Set;
 
-public interface PetService {
+public interface PetService extends CrudService<Pet,Long> {
 
-    Pet findById(Long id);
-    Pet save(Pet owner);
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/venjava/spring/vkspetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/venjava/spring/vkspetclinic/services/VetService.java
@@ -2,10 +2,7 @@ package venjava.spring.vkspetclinic.services;
 
 import venjava.spring.vkspetclinic.model.Vet;
 
-import java.util.Set;
 
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet owner);
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet,Long>{
+
 }
